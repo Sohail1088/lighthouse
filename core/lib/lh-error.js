39c4b9ec2df0c7535dd7747b37484e327ae81ca5
Adam Raine <ad.st.raine@gmail.com>
2023-06-20 12:54:15 -0700

    core: support ts targets before es2022

--- core/lib/lh-error.js
+++ core/lib/lh-error.js
@@ -111,11 +111,16 @@ const ERROR_SENTINEL = '__ErrorSentinel';
  * @typedef {{sentinel: '__ErrorSentinel', message: string, code?: string, stack?: string, cause?: unknown}} SerializedBaseError
  */
 
+/**
+ * The {@link ErrorOptions} type wasn't added until es2022 (Node 16), so we recreate it here to support ts targets before es2022.
+ * @typedef {{cause: unknown}} LHErrorOptions
+ */
+
 class LighthouseError extends Error {
   /**
    * @param {LighthouseErrorDefinition} errorDefinition
    * @param {Record<string, string|undefined>=} properties
-   * @param {ErrorOptions=} options
+   * @param {LHErrorOptions=} options
    */
   constructor(errorDefinition, properties, options) {
     super(errorDefinition.code, options);
